9c0655713255146922bfe65dc1db49e40ed281b1
Catherine-YQ <38750378+Catherine-YQ@users.noreply.github.com>
2018-08-02 20:08:25 +0800

    Add files via upload

---
+++ Assets/PropEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropEventManager : MonoBehaviour {
+    public bool[] Propactive;
+    public GameObject Dialogbox;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Prop1()
+    {
+        Dialogbox.SetActive(true);
+        GetComponent<>
+    }
+}
---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour {
+    private Transform origin;
+	// Use this for initialization
+	void Start () {
+        origin.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);   // // 获取目标对象当前的世界坐标系位置，并将其转换为屏幕坐标系的点
+        // 设置鼠标的屏幕坐标向量，用上面获得的Pos的z轴数据作为鼠标的z轴数据，使鼠标坐标 // 与目标对象坐标处于同一层面上
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos.z);
+        // 用上面获取到的鼠标坐标转换为世界坐标系的点，并用其设置目标对象的当前位置
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void OnMouseUp()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, -Vector2.up);
+        if (hit.collider.gameObject.tag == "prop1_belonging"&&gameObject.tag=="prop1")
+        {
+            
+        }
+        else
+        { } //如果不是格子或没有检测到物体，则将物品放回到原来的格子内 transform.parent=originalGrid.transform; } } else { transform.parent=originalGrid.transform; }
+    }
+
+}
